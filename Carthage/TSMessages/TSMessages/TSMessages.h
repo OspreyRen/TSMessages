7fdf02503b0d9342b099537430d08720e04d01ac
ospreyren <58637717@qq.com>
2015-08-11 17:39:26 +0800

    support carthage

---
+++ Carthage/TSMessages/TSMessages/TSMessages.h
@@ -0,0 +1,12 @@
+//
+//  TSMessages.h
+//  TSMessages
+//
+//  Created by ospreyren on 8/11/15.
+//  Copyright (c) 2015 ospreyren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TSMessage.h"
+#import "TSMessageView.h"
+#import "TSBlurView.h"
